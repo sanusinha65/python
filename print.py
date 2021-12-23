eba8437285eb6c1e373edfdd626f65baa086f4b8
sanusinha65 <sanusinha65@gmail.com>
2021-12-23 19:44:29 +0530

    Print Function

---
+++ print.py
@@ -0,0 +1,2 @@
+print("Welcome To The Python Basic")  #print is a function which executes the command on the line 
+#the "Welcome To The Python Basic" is the string which will be printed when we will run the command
