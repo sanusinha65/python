da58bcf369a56cc7da2152e2b6a6b4676f034f5e
sanusinha65 <sanusinha65@gmail.com>
2021-12-24 12:19:50 +0530

    Print Even Natural Number

---
+++ print even numbers.py
@@ -0,0 +1,6 @@
+n= int(input("Enter The Number ")) #taking input
+even= 0 #even number starts from 2 declaring 0 so we can add 2 in every iteration 
+for i in range(0,n):      #for loop 
+    
+    print(even, end=" ")     #for printing the output
+    even +=2   #adding 2 in every iteration 
