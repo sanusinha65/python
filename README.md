808087ea6992800590ee4255001bb3f4493943b2
sanusinha65 <sanusinha65@gmail.com>
2021-12-24 12:25:29 +0530

    Printing Odd Number From Range

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python
 Basic Fundamentals
+print: print function
+even or not: It will check whether the number is even or not
+odd or not: It will check whether the number is odd or not
---
+++ print odd numbers.py
@@ -0,0 +1,6 @@
+n= int(input("Enter The Number ")) #taking input to check the number 
+odd= 1 #odd number starts from 1 
+for i in range(n):      #for loop 
+    
+    print(odd, end=" ")     #for printing the output
+    odd +=2   #adding 2 in every iteration 
