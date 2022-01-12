ddc1971585da185414be4209dad3c7ca185902c1
sanusinha65 <sanusinha65@gmail.com>
2022-01-12 16:29:54 +0530

    PYTHON FUNCTIONS

---
+++ 9. average of three numbers.py
@@ -0,0 +1,8 @@
+a= int(input("Enter The Number ")) #taking input to check the number 
+b= int(input("Enter The 2nd Number ")) #taking input to check the number 
+c= int(input("Enter The 3rd Number ")) #taking input to check the number 
+
+sum= a+b+c      #adding the numbers
+divide= sum/3    #dividing it by the total no. 
+
+print(divide, "is average of the three numbers")
