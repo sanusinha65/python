e08c3cee677b9d4008905e1d7a9785c7987fe4f6
sanusinha65 <sanusinha65@gmail.com>
2022-01-12 16:42:06 +0530

    PYTHON FUNCTIONS

---
+++ 13. cube numbers.py
@@ -0,0 +1,4 @@
+n = int(input("Enter numbers to get cube "))  #taking input 
+#for loop starts here 
+for i in range(1, n + 1):
+    print(i*i*i) #printing the output
---
+++ 14. particular cube of number.py
@@ -0,0 +1,3 @@
+n = int(input("Enter the number to get cube "))  #taking input 
+d= n**3 #storing variable for n **3 means n*n*n
+print(d) #printing the output
