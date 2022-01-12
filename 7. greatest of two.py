b82b750332f8b87e100408a2e7160b7f57f9c947
sanusinha65 <sanusinha65@gmail.com>
2022-01-12 16:26:16 +0530

    PYTHON FUNCTIONS

---
+++ 7. greatest of two.py
@@ -0,0 +1,7 @@
+a= int(input("Enter The Number ")) #taking input to check the number 
+b= int(input("Enter The 2nd Number ")) #taking input to check the number 
+
+if a>b: #if condition for checking both the numbers 
+    print(a, "is greater number")  #if the condition is true then this number will be printed 
+else:
+    print(b, "is greater number") #if the first condition is false then the else condition will be printed
